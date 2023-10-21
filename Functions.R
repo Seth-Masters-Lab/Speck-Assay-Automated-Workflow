908a89fa35d6f025c17760aaae2314fc5fd1198e
mattwiz101 <148309728+mattwiz101@users.noreply.github.com>
2023-10-21 15:18:47 +1100

    Update Functions.R
    Added import function

--- Functions.R
+++ Functions.R
@@ -1,8 +1,37 @@
+################################################################################
 
+
+
+## Import FSC files and perform cleanup if desired
+
+fcsImport <- function(path, clean, logTrans){
+  
+  # Load Data
+  myfiles <- list.files(path = path, pattern = ".fcs", ignore.case = T)
+  fs <<- read.flowSet(myfiles, path = path, alter.names = T)
+  
+  # Assign well ID to samples
+  pData(fs)$well <- gsub(".*_.*_(.*)_.*.fcs","\\1",sampleNames(fs))
+  
+  # Data Cleaning
+  if(clean == T){
+    fs <<- flow_auto_qc(fs, mini_report = F, html_report = F, fcs_QC = F, folder_results = F)}
+  
+  # Log transformation
+  if(logTrans == T){
+    print(fs[[1]]@parameters@data[1])
+    logRange <- readline("Please input the channels to be converted to log scale, (eg. 5:10): ")
+    lower <- gsub(x = logRange, pattern = ":.*", replacement = "")
+    upper <- gsub(x = logRange, pattern = "*.:", replacement = "")
+    
+    trans <- estimateLogicle(fs[[1]], colnames(fs[,strtoi(lower):strtoi(upper)]))
+    fs <<- transform(fs, trans)}
+}
 
 
 
 ################################################################################
+
 
 
 ## Step Gating ##
