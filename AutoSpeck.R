210b7a798fe731f062a968f881bac8b2f813a5dd
mattwiz101 <148309728+mattwiz101@users.noreply.github.com>
2023-10-23 19:37:07 +1100

    Update AutoSpeck.R

--- AutoSpeck.R
+++ AutoSpeck.R
@@ -13,17 +13,18 @@ source('Functions.R')
 
 path <- 'Testing_NLRP3_library_12_sample_1A-H'
 
+
 fs <- fcsImport(path, T, T)
 
 # Create an empty gating set
 gs <- GatingSet(fs)
 
 # QA Step
-ggcyto(gs, subset = 'root', aes(x = 'FSC.A', y = 'SSC.A')) + geom_hex(bins = 100)
+# ggcyto(gs, subset = 'root', aes(x = 'FSC.A', y = 'SSC.A')) + geom_hex(bins = 100)
 
 # Debris Gate
 gate1d(gatingSet = gs, parentPop = 'root', xchannel = 'FSC.A', name = 'debris',
-       plot = F, positive = T, range = c(0,1e5), smoothing = 2, peaks = NULL)
+       plot = T, positive = T, range = c(0,1e5), smoothing = 2, peaks = NULL)
 
 #Single cell gates
 gate1d(gs, 'debris', xchannel = 'SSC.W', range = c(0,5), name = 'single1', positive = F, plot = T, smoothing = 2, peaks = NULL)
