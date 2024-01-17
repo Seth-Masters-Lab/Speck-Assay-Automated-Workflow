fdf3935fbd59d04c891ae8c2853f8418ae3dbd69
Matthew Wierzbowski <148309728+mattwiz101@users.noreply.github.com>
2024-01-17 13:56:19 +1100

    Update Packages.R

--- Packages.R
+++ Packages.R
@@ -1,6 +1,8 @@
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
+install.packages("shiny")
+
 BiocManager::install("flowAI")
 BiocManager::install("flowCore")
 BiocManager::install("flowWorkspace")
